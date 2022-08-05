b7e2d38b48a3848529f4158996f274ca4bbe9f2e
abhishek <abbhishekk27@gmail.com>
2022-08-05 21:09:49 +0530

    next app init

---
+++ app/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import Image from 'next/image';
+// import styles from '../styles/Home.module.css'
+import { Box, Text, Flex } from '@chakra-ui/react';
+
+export default function Home() {
+	return (
+		<>
+			<Text>Hello World</Text>
+		</>
+	);
+}
